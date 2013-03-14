4ed230a87f16466e5c8966379ca51ad1ac7826aa
Thomas Stroemme <strtho@gmail.com>
2013-03-14 10:41:33 +0100

    Adding rails app with needed gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lokalvogn::Application.config.secret_token = '04f77d3a672ff6362ac2deda2e3297a75fe0ddd53eafd8a066f42b6f8f0c076991a98764522e864c0b7240c7bb50bdc6ccdf63d94dc3f73128acde2741ca60fb'
